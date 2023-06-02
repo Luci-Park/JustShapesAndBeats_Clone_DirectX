ec9b2332734a215ab76910d577a465c179090758
luciPark <97658764+Luci-Park@users.noreply.github.com>
2023-06-02 15:04:53 +0900

    6/1

--- LuciEngine/Engine_SOURCE/LGraphics.h
+++ LuciEngine/Engine_SOURCE/LGraphics.h
@@ -1,2 +1,18 @@
 #pragma once
 
+enum class eShaderStage
+{
+	VS, //Vertex Shader
+	HS, //Hull Shader
+	DS, //Domain Shader
+	GS, //Geometry shader
+	PS, //Pixel Shader
+	CS, //Compute Shader
+	None
+};
+
+enum class eCBType
+{
+	Transform,
+	None
+};
--- LuciEngine/Shader_SOURCE/TriangleVS.hlsl
+++ LuciEngine/Shader_SOURCE/TriangleVS.hlsl
@@ -11,13 +11,18 @@ struct VSOut
     float4 Color : COLOR;
 };
 
+cbuffer Transform : register(b0)
+{
+    float4 Pos;
+}
+
 VSOut main(VSIn In)
 {
     VSOut Out = (VSOut)0.0f;
 
-
-
     Out.Pos = float4(In.Pos, 1.0f);
+    Out.Pos.x += Pos.x;
+    
     Out.Color = In.Color;
 
     return Out;
