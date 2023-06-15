63cea17717b478619350ebea000eed396518fe68
luciPark <97658764+Luci-Park@users.noreply.github.com>
2023-06-15 09:38:47 +0900

    Update LGraphicDevice_Dx11.cpp

--- LuciEngine/Engine_SOURCE/LGraphicDevice_Dx11.cpp
+++ LuciEngine/Engine_SOURCE/LGraphicDevice_Dx11.cpp
@@ -320,9 +320,9 @@ namespace lu::graphics
 		BindViewPort(&mViewPort);
 		mContext->OMSetRenderTargets(1, mRenderTargetView.GetAddressOf(), mDepthStencilView.Get());
 
-		//renderer::mesh->BindBuffer();
-		//renderer::shader->Binds();
-		//DrawIndexed(renderer::mesh->GetIndexCount(), 0, 0);
+		renderer::mesh->BindBuffer();
+		renderer::shader->Binds();
+		DrawIndexed(renderer::mesh->GetIndexCount(), 0, 0);
 	}
 
 	void GraphicDevice_Dx11::Present()
