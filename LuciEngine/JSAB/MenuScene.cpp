b69ea1562f222adc7708e06ac45ad5525e6a2056
suminp96 <97658764+Luci-Park@users.noreply.github.com>
2023-07-03 23:26:15 +0900

    Update MenuScene.cpp

--- LuciEngine/JSAB/MenuScene.cpp
+++ LuciEngine/JSAB/MenuScene.cpp
@@ -2,5 +2,22 @@
 
 namespace lu::JSAB
 {
-
+	MenuScene::MenuScene()
+	{
+	}
+	MenuScene::~MenuScene()
+	{
+	}
+	void MenuScene::Initialize()
+	{
+	}
+	void MenuScene::Update()
+	{
+	}
+	void MenuScene::LateUpdate()
+	{
+	}
+	void MenuScene::Render()
+	{
+	}
 }
