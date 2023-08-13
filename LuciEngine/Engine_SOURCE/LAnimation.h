21adf0e306bba62f06044e4aa80310a56ed58808
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2023-08-13 18:47:42 +0900

    Update LAnimation.h

--- LuciEngine/Engine_SOURCE/LAnimation.h
+++ LuciEngine/Engine_SOURCE/LAnimation.h
@@ -1,14 +1,40 @@
 #pragma once
 #include "LResource.h"
+#include "LTexture.h"
 namespace lu
 {
 	class Animator;
 	class Animation : public Resource
 	{
 	public:
+		struct KeyFrame
+		{
+			float mTime;
+			union
+			{
+				std::function<void()>func;
+				graphics::Texture textValue;
+				math::Color colorValue;
+				math::Vector3 vector3Value;
+				math::Vector2 vector2Value;
+				math::Quaternion quatValue;
+				float floatValue;
+				bool boolValue;
+			};
+		};
+		enum eAnimationType
+		{
+			TrPosition, TrScale, TrRotation, TrLocalPosition, TrLocalScale, TrLocalRotation,
+			CdCenter, CdSize, CdRotation, CdActive,
+			MrTexture, MrColor, MrInterpolation, MrTint, MrActive,
+			ScFunc
+		};
+
+	public:
 		Animation();
 		~Animation();
 		virtual HRESULT Load(const std::wstring& path) { return S_FALSE; }
+		void AddKeyFrame(eAnimationType type, KeyFrame keyFrame);
 
 		void Update();
 		void LateUpdate();
@@ -18,8 +44,10 @@ namespace lu
 		bool IsComplete() { return mbComplete; }
 
 	private:
+
 		Animator* mAnimator;
 		float mTime;
+		float mDuration;
 		bool mbComplete;
 	};
 }
