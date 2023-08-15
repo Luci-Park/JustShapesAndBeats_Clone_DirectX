ea45bcaca8db1fc564f22ac208557f7f59dc202c
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2023-08-15 20:12:00 +0900

    Revert "Animation Types"
    This reverts commit 8b1845e7050dc4df73ab8ec471f5e426b0b9f4f8.

--- LuciEngine/Engine_SOURCE/LAnimation.cpp
+++ LuciEngine/Engine_SOURCE/LAnimation.cpp
@@ -1,53 +1,19 @@
 #include "LAnimation.h"
-#include "LTime.h"
-#include "LGameObject.h"
-#include "LTransform.h"
-#include "LCollider2D.h"
-#include "LMeshRenderer.h"
+
 namespace lu
 {
-	Animation::Animation(GameObject* owner)
+	Animation::Animation()
 		:Resource(enums::eResourceType::Animation)
 	{
-		mAni = owner->GetComponent <Animator>();
-		mTr = owner->GetComponent<Transform>();
-		mCd = owner->GetComponent<Collider2D>();
-		mMr = owner->GetComponent<MeshRenderer>();
-		mTimelines = std::vector<Timeline*>((UINT)eAnimationType::End, nullptr);
-		SetAnimationFunctions();
 	}
 	Animation::~Animation()
 	{
-		for (int i = 0; i < (UINT)eAnimationType::End; i++)
-		{
-			if (mTimelines[i])
-			{
-				delete mTimelines[i];
-				mTimelines[i] = nullptr;
-			}
-		}
 	}
 	void Animation::Update()
 	{
 	}
 	void Animation::LateUpdate()
 	{
-		if (mbComplete) return;
-
-		mTime += Time::DeltaTime();
-		for (int i = 0; i < (UINT)eAnimationType::End; i++)
-		{
-			Timeline* timeline = mTimelines[i];
-			if (timeline == nullptr) continue;
-			if (timeline->IsComplete()) continue;
-
-			//do something
-			
-		}
-		if (mTime > mDuration)
-		{
-			mbComplete = true;
-		}
 	}
 	void Animation::Render()
 	{
@@ -56,28 +22,5 @@ namespace lu
 	{
 		mTime = 0.0f;
 		mbComplete = false;
-	}
-	void Animation::AnimTrPos(Timeline* timeline)
-	{
-	}
-	void Animation::SetAnimationFunctions()
-	{
-		animFunctions = std::vector<std::function<void(Timeline * timeline)>>((UINT)eAnimationType::End);
-		animFunctions[(UINT)eAnimationType::TrPosition] = ([this](Timeline* timeline) { this->AnimTrPos(timeline); });
-		animFunctions[(UINT)eAnimationType::TrScale] = ([this](Timeline* timeline) { this->AnimTrScale(timeline); });
-		animFunctions[(UINT)eAnimationType::TrRotation] = ([this](Timeline* timeline) { this->AnimTrRot(timeline); });
-		animFunctions[(UINT)eAnimationType::TrLocalPosition] = ([this](Timeline* timeline) { this->AnimTrLocPos(timeline); });
-		animFunctions[(UINT)eAnimationType::TrLocalScale] = ([this](Timeline* timeline) { this->AnimTrLocScale(timeline); });
-		animFunctions[(UINT)eAnimationType::TrLocalRotation] = ([this](Timeline* timeline) { this->AnimTrLocRot(timeline); });
-		animFunctions[(UINT)eAnimationType::CdCenter] = ([this](Timeline* timeline) { this->AnimCdCenter(timeline); });
-		animFunctions[(UINT)eAnimationType::CdSize] = ([this](Timeline* timeline) { this->AnimCdSize(timeline); });
-		animFunctions[(UINT)eAnimationType::CdRotation] = ([this](Timeline* timeline) { this->AnimCdRotation(timeline); });
-		animFunctions[(UINT)eAnimationType::CdActive] = ([this](Timeline* timeline) { this->AnimCdActive(timeline); });
-		animFunctions[(UINT)eAnimationType::MrTexture] = ([this](Timeline* timeline) { this->AnimMrText(timeline); });
-		animFunctions[(UINT)eAnimationType::MrColor] = ([this](Timeline* timeline) { this->AnimMrColor(timeline); });
-		animFunctions[(UINT)eAnimationType::MrInterpolation] = ([this](Timeline* timeline) { this->AnimMrColorpolation(timeline); });
-		animFunctions[(UINT)eAnimationType::MrTint] = ([this](Timeline* timeline) { this->AnimMrTint(timeline); });
-		animFunctions[(UINT)eAnimationType::MrActive] = ([this](Timeline* timeline) { this->AnimTrActive(timeline); });
-		animFunctions[(UINT)eAnimationType::ScFunc] = ([this](Timeline* timeline) { this->AnimScFunc(timeline); });
 	}
 }
--- LuciEngine/Engine_SOURCE/LAnimation.h
+++ LuciEngine/Engine_SOURCE/LAnimation.h
@@ -3,29 +3,17 @@
 #include "LTexture.h"
 namespace lu
 {
-	class GameObject;
-	class Transform;
-	class Collider2D;
 	class Animator;
-	class MeshRenderer;
 	class Animation : public Resource
 	{
 	public:
-		enum class eAnimationType
-		{
-			TrPosition, TrScale, TrRotation, TrLocalPosition, TrLocalScale, TrLocalRotation,
-			CdCenter, CdSize, CdRotation, CdActive,
-			MrTexture, MrColor, MrInterpolation, MrTint, MrActive,
-			ScFunc,
-			End
-		};
 		struct KeyFrame
 		{
-			float timestamp;
+			float mTime;
 			union
 			{
 				std::function<void()>func;
-				std::shared_ptr<graphics::Texture> textureValue;
+				graphics::Texture textValue;
 				math::Color colorValue;
 				math::Vector3 vector3Value;
 				math::Vector2 vector2Value;
@@ -33,23 +21,20 @@ namespace lu
 				float floatValue;
 				bool boolValue;
 			};
-			bool operator<(const KeyFrame& other) const
-			{
-				return timestamp < other.timestamp;
-			}
 		};
-
-		struct Timeline
+		enum eAnimationType
 		{
-			int currIndex = 0;
-			std::vector<KeyFrame> keyframes;
-			bool IsComplete() { return currIndex >= keyframes.size(); }
+			TrPosition, TrScale, TrRotation, TrLocalPosition, TrLocalScale, TrLocalRotation,
+			CdCenter, CdSize, CdRotation, CdActive,
+			MrTexture, MrColor, MrInterpolation, MrTint, MrActive,
+			ScFunc
 		};
 
 	public:
-		Animation(GameObject* owner);
+		Animation();
 		~Animation();
 		virtual HRESULT Load(const std::wstring& path) { return S_FALSE; }
+		void AddKeyFrame(eAnimationType type, KeyFrame keyFrame);
 
 		void Update();
 		void LateUpdate();
@@ -57,34 +42,13 @@ namespace lu
 		
 		void Reset();
 		bool IsComplete() { return mbComplete; }
+
 	private:
-		void AnimTrPos(Timeline* timeline);
-		void AnimTrScale(Timeline* timeline);
-		void AnimTrRot(Timeline* timeline);
-		void AnimTrLocPos(Timeline* timeline);
-		void AnimTrLocScale(Timeline* timeline);
-		void AnimTrLocRot(Timeline* timeline);
-		void AnimCdCenter(Timeline* timeline);
-		void AnimCdSize(Timeline* timeline);
-		void AnimCdRotation(Timeline* timeline);
-		void AnimCdActive(Timeline* timeline);
-		void AnimMrText(Timeline* timeline);
-		void AnimMrColor(Timeline* timeline);
-		void AnimMrColorpolation(Timeline* timeline);
-		void AnimMrTint(Timeline* timeline);
-		void AnimTrActive(Timeline* timeline);
-		void AnimScFunc(Timeline* timeline);
-		void SetAnimationFunctions();
-	private:
+
+		Animator* mAnimator;
 		float mTime;
 		float mDuration;
 		bool mbComplete;
-		std::vector<Timeline*> mTimelines;
-		std::vector<std::function<void(Timeline* timeline)>> animFunctions;
-		Animator* mAni;
-		Transform* mTr;
-		Collider2D* mCd;
-		MeshRenderer* mMr;
 	};
 }
 
