71d0e43bf7440d86cf4b9fff180817c73eb11ee2
luciPark <97658764+Luci-Park@users.noreply.github.com>
2023-08-30 14:46:05 +0900

    Stage5

--- LuciEngine/JSAB/TutorialBeatCircle.h
+++ LuciEngine/JSAB/TutorialBeatCircle.h
@@ -1,22 +1,28 @@
 #pragma once
 #include "Bullet.h"
+
 namespace lu::JSAB
 {
 	class TutorialBeatCircle : public Bullet
 	{
 	public:
-		TutorialBeatCircle(){}
-		virtual ~TutorialBeatCircle(){}
+		TutorialBeatCircle() {}
+		virtual ~TutorialBeatCircle() {}
 		virtual void Initialize()override;
-		void SetTime(double targetTime, double currentTime);
 	private:
+		virtual void OnShow();
 		virtual void OnActivate();
 		virtual void OnDeActivate();
 		virtual void WhileActive();
 		virtual void WhileDeActive();
 
 		void CreateOnBeatAnimation();
-
+	private:
+		MeshRenderer* mMr;
+		Collider2D* mCol;
+		Animator* mAnim;
+		std::shared_ptr<Texture> mReadySprite;
+		std::shared_ptr<Texture> mActivateSprite;
 	};
 }
 
