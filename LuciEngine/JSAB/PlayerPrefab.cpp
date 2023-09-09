30dff84598fe03daccf17c00260987c6adb2e114
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2023-09-09 14:01:08 +0900

    Update PlayerPrefab.cpp

--- LuciEngine/JSAB/PlayerPrefab.cpp
+++ LuciEngine/JSAB/PlayerPrefab.cpp
@@ -5,6 +5,7 @@
 #include "Player.h"
 #include "LObject.h"
 #include "LAnimator.h"
+#include "LParticleSystem.h"
 namespace lu::JSAB
 {
 	PlayerPrefab::PlayerPrefab(eLayerType layer)
@@ -27,6 +28,26 @@ namespace lu::JSAB
 		AddComponent<Collider2D>();
 
 		Player* script = AddComponent<Player>();
+		{
+			auto p = AddComponent<ParticleSystem>();
+			p->GetMaterial()->SetTexture(Resources::Find<Texture>(L"player1"));
+			p->Duration = 10;
+			p->MaxParticles = 1000;
+			p->Loop = true;
+			p->RateOverTime = 0;
+			p->RateOverDistance = 0.5;
+
+			p->SetOffset(Vector3::Zero);
+			p->SetAngle(60, -60);
+			p->SetLifeTime(1);
+			p->SetSize(10, 0);
+			p->SetTint({ 1, 1, 1, 0.5 }, { 0, 0, 1, 0.5 });
+			p->SetWorldSpace(ParticleSystem::Space::world);
+			p->SetGravity(0);
+			p->SetStartSpeed(20);
+			p->Play();
+		}
+
 
 		{
 			GameObject* dashoutline = object::Instantiate<GameObject>(mTransform, eLayerType::Player);
